c56987d6e3bc539e849bdd649123bb96dc7c1b54
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-04-20 16:58:20 +0200

    readd occultism support

--- src/main/java/bl4ckscor3/mod/ceilingtorch/CompatConfig.java
+++ src/main/java/bl4ckscor3/mod/ceilingtorch/CompatConfig.java
@@ -10,6 +10,7 @@ import bl4ckscor3.mod.ceilingtorch.compat.adorn.AdornCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.bambooeverything.BambooEverythingCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.bonetorch.BoneTorchCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.chipped.ChippedCompat;
+import bl4ckscor3.mod.ceilingtorch.compat.occultism.OccultismCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.tofucraft.TofuCraftCompat;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.config.ModConfig;
@@ -52,8 +53,9 @@ public class CompatConfig {
 		builtInCompat = Map.ofEntries(
 				makeEntry(builder, "adorn", () -> AdornCompat::new),
 				makeEntry(builder, "bambooeverything", () -> BambooEverythingCompat::new),
-				makeEntry(builder, "chipped", () -> ChippedCompat::new),
 				makeEntry(builder, "bonetorch", () -> BoneTorchCompat::new),
+				makeEntry(builder, "chipped", () -> ChippedCompat::new),
+				makeEntry(builder, "occultism", () -> OccultismCompat::new),
 				makeEntry(builder, "tofucraft", () -> TofuCraftCompat::new));
 		//@formatter:on
 	}
