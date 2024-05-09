699e3e4efa878b9b1e82dcfbd7507fb267c9cd95
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-05-09 10:58:17 +0200

    add soul bonetorch support

--- src/main/java/bl4ckscor3/mod/ceilingtorch/compat/bonetorch/BoneTorchCompat.java
+++ src/main/java/bl4ckscor3/mod/ceilingtorch/compat/bonetorch/BoneTorchCompat.java
@@ -16,12 +16,13 @@ import net.neoforged.neoforge.registries.DeferredBlock;
 
 public class BoneTorchCompat implements ICeilingTorchCompat {
 	public static final DeferredBlock<CeilingTorchBlock> CEILING_BONE_TORCH = CeilingTorch.BLOCKS.register("bonetorch_bonetorch", () -> new CeilingTorchBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.TORCH), ParticleTypes.FLAME, BoneTorchMod.BONETORCH));
+	public static final DeferredBlock<CeilingTorchBlock> CEILING_SOUL_BONE_TORCH = CeilingTorch.BLOCKS.register("bonetorch_soul_bonetorch", () -> new CeilingTorchBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.SOUL_TORCH), ParticleTypes.SOUL_FIRE_FLAME, BoneTorchMod.SOUL_BONETORCH));
 	private Map<ResourceLocation, Block> placeEntries;
 
 	@Override
 	public Map<ResourceLocation, Block> getPlaceEntries() {
 		if (placeEntries == null)
-			placeEntries = Map.of(BoneTorchMod.BONETORCH.getId(), CEILING_BONE_TORCH.get());
+			placeEntries = Map.of(BoneTorchMod.BONETORCH.getId(), CEILING_BONE_TORCH.get(), BoneTorchMod.SOUL_BONETORCH.getId(), CEILING_SOUL_BONE_TORCH.get());
 
 		return placeEntries;
 	}
